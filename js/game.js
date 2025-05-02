6bb2104a42f73148aa95c3c41bbeb71976217f21
voirindev <yves.voirin@geoimagesolutions.com>
2025-05-02 12:05:41 -0400

    Update game mechanics and assets: add spider and fly sprites, implement scoring, and adjust gravity settings

--- js/game.js
+++ js/game.js
@@ -1,23 +1,55 @@
 export default class GameScene extends Phaser.Scene {
+
     constructor() {
       super('GameScene');
+      this.score = 0;
     }
   
     preload() {
-      this.load.image('player', 'assets/images/player.png');
-      this.load.audio('jump', 'assets/audio/jump.wav');
+        this.load.image('spider', 'assets/images/orange-spider.png');
+        this.load.image('fly', 'assets/images/fly.png');
     }
   
     create() {
-      this.player = this.physics.add.sprite(100, 450, 'player');
-      this.input.keyboard.on('keydown-SPACE', () => {
-        this.player.setVelocityY(-300);
-        this.sound.play('jump');
-      });
+        // add the spider
+        this.spider = this.physics.add.sprite(200, 300, 'spider'); // adding the logo
+        this.spider.scale = 0.25;
+        
+        // add the fly
+        this.fly = this.physics.add.sprite(600, 400, 'fly'); // adding the logo
+        this.fly.scale = 0.12;
+        
+        // add the score text
+        this.mytext = this.add.text(10, 10, 'Score: '+this.score, { fontSize: '22px', fill: '#fff' })
+        
+        // add the cursor keys
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        // add the overlap between the spider and the fly
+        this.physics.add.overlap(
+            this.spider,
+            this.fly,
+            (spider, fly) => {
+            fly.disableBody(true, true)
+                this.score ++;
+                this.mytext.setText("Score: " + this.score)
+            },
+            null,
+            this
+        )
     }
   
     update() {
-      // update game logic here
+        if(this.cursors.right.isDown) {
+            this.spider.x += 1;
+        }
+        else if(this.cursors.left.isDown) {
+            this.spider.x -= 1;
+        } else if(this.cursors.up.isDown) {
+            this.spider.y -= 1;
+        } else if(this.cursors.down.isDown) {
+            this.spider.y += 1;
+        }
     }
   }
   
--- js/main.js
+++ js/main.js
@@ -5,11 +5,12 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  backgroundColor: '#bfcc00',
   scene: GameScene,
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 300 },
+      gravity: { y: 0 },
       debug: true
     }
   }
